b5cf3a5443191912563b3e4e0bddc11148a2b0c0
Khende <112852648+Khende@users.noreply.github.com>
2022-11-15 12:32:40 +0100

    Add files via upload

---
+++ php_gyakorlas/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC modell</title>
+</head>
+<body>
+<h1>Menü</h1>
+<nav>
+    <ul>
+        <li><a href="index.php?menu=home">Home</a></li>
+        <li><a href="index.php?menu=contact">Contact</a></li>
+        <li><a href="index.php?menu=about">About</a></li>
+
+
+    </ul>
+</nav>
+<?php
+require_once "./controller"
+
+
+?>
+
+
+</body>
+</html>
